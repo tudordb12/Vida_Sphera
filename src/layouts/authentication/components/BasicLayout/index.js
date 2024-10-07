b0ea2ee74724c087a01e26962099e5b38e505272
Tudor D.B <tudordurdb20734@gmail.com>
2024-10-07 23:27:46 +0300

    Changes to be committed:

--- src/layouts/authentication/components/BasicLayout/index.js
+++ src/layouts/authentication/components/BasicLayout/index.js
@@ -23,8 +23,8 @@ function BasicLayout({ title, description, image, children }) {
       <DefaultNavbar
         action={{
           type: "external",
-          route: "https://creative-tim.com/product/vision-ui-dashboard-react",
-          label: "Free Download",
+          route: "",
+          label: "",
         }}
         transparent
         light
